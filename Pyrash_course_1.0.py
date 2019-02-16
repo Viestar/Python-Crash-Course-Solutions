c299ab873620fb38e50b445947643bcfdf906093
slysch <s.schmitzerx@gmail.com>
2019-02-16 10:28:04 +0300

    First commitment to the project

---
+++ Pyrash_course_1.0.py
@@ -0,0 +1,23 @@
+# if __author__  = "__slysch__":
+    # CHALLENGE:
+        # Write a program that outputs "Hello, Python World!"
+
+
+"""
+APPROACH:
+You simply have to call/use a "print()" method/function and inside its parenthesis,
+add whatever you wish/desire/want printed out BUT considering first its type. 
+And in this case we have a STRING, "Hello, Python world".
+(Same applies to other data types)
+"""
+
+# PROGRAM
+print("Hello, Python World!")
+
+
+
+# TAKE AWAY
+print("You are going to be number") #string
+print(1) #Integer
+print("in 1000 python snakes. ") #string with numbers in it
+
