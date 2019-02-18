589c754b65b578f81ee0e378d5549beb00d2e687
slysch <s.schmitzerx@gmail.com>
2019-02-18 20:27:49 +0300

    Add try it yourself 1.1 and 1.2

---
+++ Pyrash_course_1.1.py
@@ -0,0 +1,6 @@
+"""1-1. python.org: Explore the Python home page (http://python.org/) to find
+topics that interest you. As you become familiar with Python, different parts of
+the site will be more useful to you."""
+
+
+# NEED I SAY MORE?
---
+++ Pyrash_course_1.2.py
@@ -0,0 +1,20 @@
+# 1-2. Hello World Typos: Open the hello_world.py(Pyrash_course_1.0.py in this instance)
+# file you just created. Make a typo somewhere in the line and run the program again.
+# Can you make a typo that generates an error? Can you make sense of the error message?
+# Can you make a typo that doesn’t generate an error?
+# Why do you think it didn’t make an error?
+
+"""In programming a "typo" is more like a mistake in grammar of a language. its not directly directed
+to guys wwith blood type O :D,
+Here we are simply asked to make a mistake in our syntax that the intepreter will raise an ERROR
+and another tolerable error that wont raise any flags.
+"""
+
+# PROGRAM WITH A TYPO THAT GENERATES AN ERROR
+    print("Hello, Python World!")
+
+# PROGRAM WITH A TYPO THAT DOESN'T GENERATE AN ERROR
+print   ("Hello, Python World!")
+
+
+# I HOPE I GOT THIS RIGHT
