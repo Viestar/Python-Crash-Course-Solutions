85f172245744768ca30e55e5835bb3ebdd1e01b8
slysch <s.schmitzerx@gmail.com>
2019-02-20 17:44:00 +0300

    Try it yourself 2.2

---
+++ Pyrash_course_2.2.py
@@ -0,0 +1,25 @@
+# if __author__  = "__slysch__":
+    # store a message in a variable,
+    # print it out and re assign the variable again.
+
+
+"""
+Here we are being asked to tackle variables, remember variables are like stores, so we store the
+value inside a variable and we keep calling the variable whenever we want the value printed instead
+of inserting a long statement inside the function/method's parentheses for the many times we are
+asked to print it, It woruld really be tiresome and we don't like repeating ourselves.
+NOTE: Variable values can be changed AND once changed, its gone permanently.
+Same way as once you've stored certain things in your store and eventually replaced
+them with others. They like gone and now your store has completely different values.
+"""
+
+
+message = "Hello Python, Please be nice to me."
+print(message)
+message = "Don't worry buddy, Feel at home.  \nYours\n\tPython"
+print(message)
+
+
+
+# Take  Away
+# The "\n" and  "\t" indicate new line and tab respectively.
