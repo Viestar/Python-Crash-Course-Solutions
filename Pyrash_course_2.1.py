c0d636eb1b62902470cbe9aff1e794611c2346ad
slysch <s.schmitzerx@gmail.com>
2019-02-20 17:42:13 +0300

    Try it yourself 2.1

---
+++ Pyrash_course_2.1.py
@@ -0,0 +1,10 @@
+# if __author__  = "__slysch__":
+    # 2-1. Simple Message: Store a message in a variable, and then print that
+    # message.
+"""
+Right, like mentioned above , We SIMPLY gna store what we would've just printed out from the parenthesis
+of the function, first in a variable and then we print the variable name instead.
+This is reallllllly helpful incase we have to call the variable multiple times.
+"""
+message = "Hello, i actually feel like python is getting a little easier every time i try something out myself"
+print(message)
