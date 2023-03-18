e15193d76d28dcf6491e973535ebe3fc1f82ee8d
Viestar <m.silverbuckson@gmail.com>
2023-03-18 19:45:52 +0300

    helloworld.py

---
+++ ignore/helloworld.py
@@ -0,0 +1,29 @@
+# A function that returns the string "Hello World!"
+
+def helloworld():
+    return "Hello World!"
+
+# Saving the returned value in a variable
+hello = helloworld()
+
+# Outputting the return value
+print(hello)
+
+
+
+
+# Another function that returns a string through a variable"
+
+def helloworld():
+    # Saving the string into a variable
+    string = "Hello World!"
+    
+    # returning the variable
+    return string
+
+
+# Saving the returned value in a variable
+hello = helloworld()
+
+# Outputting the return value
+print(hello)
